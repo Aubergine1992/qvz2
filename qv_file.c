0dbdd7945426601e09cb20680b88616a4f7098d8
Mikel Hernaez <mikelhernaez@gmail.com>
2016-02-21 16:40:21 -0800

    add verbose

--- qv_file.c
+++ qv_file.c
@@ -95,6 +95,7 @@ uint32_t generate_qv_struct(struct qv_file_t* qvs, struct quality_file_t *info, 
     uint32_t status, block_idx, line_idx;
     
     printf("0\n");
+    printf("%d,%d", info->columns, qvs->read_length);
     //figure out how long the file is
     info->columns = qvs->read_length;
     if (info->columns > MAX_READS_PER_LINE) {
